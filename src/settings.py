df2da3ba114457e4b5643b50e67b5f432eaed2a2
Amin Rezaeeyan <rezaeeyanamin@gmail.com>
2024-08-24 12:40:42 +0330

    chore: add settings

---
+++ src/settings.py
@@ -0,0 +1,28 @@
+# <------< Layout >------>
+WIDTH, HEIGHT = 675, 675  # dimensions of sudoku board
+ROWS, COLS = 9, 9
+CELL_SIZE = WIDTH // COLS
+MARGIN = 5  # margin between cells
+BORDER_RADIUS = 15
+BORDER_WIDTH = 1
+CLICKED_BORDER_WIDTH = 2
+
+# <------< Colors >------>
+BG_COLOR = (255, 255, 255)
+LIGHTER_CELL_COLOR = (250, 250, 250)
+DARKER_CELL_COLOR = (240, 240, 255)
+BORDER_COLOR = (175, 175, 215)
+CLICKED_BORDER_COLOR = (20, 20, 20)
+VALID_BORDER_COLOR = (0, 255, 0)
+INVALID_BORDER_COLOR = (255, 0, 0)
+NUMBER_COLOR = (0, 0, 0)
+PREDEFINED_NUMBER_COLOR = (120, 120, 220)
+TEXT_COLOR = (150, 150, 200)
+TIMER_COLOR = (0, 0, 120)
+
+# <------< Fonts >------>
+NUMBER_FONT_PATH = '../resources/fonts/Pacifico.ttf'
+NUMBER_FONT_SIZE = 32
+TEXT_FONT_PATH = '../resources/fonts/NewAmsterdam.ttf'
+TEXT_FONT_SIZE = 26
+TIMER_FONT_SIZE = 36
